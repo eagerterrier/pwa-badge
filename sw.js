3ee64e52507da496f46b2402173843df07eb92ff
eagerterrier <eagerterrier@gmail.com>
2024-10-11 16:59:00 +0100

    ENH: colors and beginning of sw

---
+++ sw.js
@@ -0,0 +1,17 @@
+// Listen to "push" events in the service worker.
+self.addEventListener("push", (event) => {
+  // Extract the unread count from the push message data.
+  const message = event.data.json();
+  const unreadCount = message.unreadCount;
+
+  // Set or clear the badge.
+  if (navigator.setAppBadge) {
+    if (unreadCount && unreadCount > 0) {
+      navigator.setAppBadge(unreadCount);
+    } else {
+      navigator.clearAppBadge();
+    }
+  }
+  // It's obligatory to show the notification to the user.
+  self.registration.showNotification(`${unreadCount} unread messages`);
+});
