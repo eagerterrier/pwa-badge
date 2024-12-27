34ced907b4c23193e61ccd2119528c14a436d809
eagerterrier <eagerterrier@gmail.com>
2024-12-27 13:26:42 +0000

    CHG: more things

--- sw.js
+++ sw.js
@@ -1,17 +1,26 @@
-// Listen to "push" events in the service worker.
-self.addEventListener("push", (event) => {
-  // Extract the unread count from the push message data.
-  const message = event.data.json();
-  const unreadCount = message.unreadCount;
+/* eslint-disable */
+const version = 'v0.0.2';
+const cacheNumber = 0;
+const cacheName = `thingCache-${cacheNumber}`;
 
-  // Set or clear the badge.
-  if (navigator.setAppBadge) {
-    if (unreadCount && unreadCount > 0) {
-      navigator.setAppBadge(unreadCount);
-    } else {
-      navigator.clearAppBadge();
-    }
-  }
-  // It's obligatory to show the notification to the user.
-  self.registration.showNotification(`${unreadCount} unread messages`);
+const broadcast = new BroadcastChannel('count-channel');
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(cacheName));
+  updateLotteryDetails().then((data) => {
+    console.log('data');
+  });
 });
+
+self.addEventListener("periodicsync", (event) => {
+  if (event.tag === "get-latest-lottery-details") {
+    event.waitUntil(updateLotteryDetails());
+  }
+});
+
+const updateLotteryDetails = () => {
+    return new Promise((resolve) => {
+        broadcast.postMessage({ payload: "updating" });
+        resolve("updating");
+    });
+}
